a2a9d2087c9cacc82f7ad3877eda1a41d81953be
Saaya Yasuda <saaya.yasuda@gmail.com>
2017-10-29 21:27:29 -0400

    Cleaned up the multinom file

--- lib/multinom_model.r
+++ lib/multinom_model.r
@@ -1,3 +1,8 @@
+#######################################
+# Multinomial logictic regression
+# Author: Saaya Yasuda (sy2569)
+#######################################
+
 rm(list=ls())
 setwd('~/Documents/Github/Fall2017-project3-fall2017-project3-grp2')
 
@@ -13,16 +18,19 @@ if(length(packages.needed)>0){
 library(readr)
 library(nnet)
 library(caret)
-#library(gbm)
 
 #######################################
 # Load sift features + labels
 #######################################
+
+# feature files
 sift_train = read_csv("~/Downloads/training_set/sift_train.csv")
 hog_features = read_csv("./doc/hog.csv")
 
+# label
 label = read_csv("~/Downloads/training_set/label_train.csv")
 
+# merging them
 data = data.frame(label[,2], sift_train[,2:ncol(sift_train)])
 data_hog = data.frame(label[,2], hog_features[,2:ncol(hog_features)])
 
@@ -30,7 +38,7 @@ colnames(data)[1] = "label"
 colnames(data_hog)[1] = "label"
 
 #######################################
-# divide into train & test for SIFT (70:30)
+# For SIFT: divide into train & test (70:30)
 #######################################
 set.seed(123)
 index = sample(1:nrow(data), size=0.7*nrow(data))
@@ -38,7 +46,7 @@ train_data = data[index,]
 test_data = data[-index,]
 
 #######################################
-# divide into train & test for HOG (70:30)
+# For HOG: divide into train & test (70:30)
 #######################################
 set.seed(123)
 index = sample(1:nrow(data_hog), size=0.7*nrow(data_hog))
@@ -63,7 +71,6 @@ multinomfit_train = multinom_train(train_data)
 
 stepwisefit = step(multinomfit_train$fit, direction="both", 
                    scope=formula(multinomfit_train$fit))
-
 #system.time(step(multinomfit_train$fit, direction="both", 
 #                 scope=formula(multinomfit_train$fit)))
 
@@ -91,12 +98,15 @@ multinom_test <- function(test_data, fit){
   return(multinom_pred)
 }
 
-#run it:
+# run it:
 multinomtest_result = multinom_test(test_data,multinomfit_train$fit)
 postResample(test_data$label,multinomtest_result)
 
+# only for stepwise
 multinomtest_result_stepwise = multinom_test(test_data,stepwisefit)
 postResample(test_data$label,multinomtest_result_stepwise)
+
+#confusionMatrix(test_data$label,multinomtest_result)
 
 #######################################
 # Error rate / Accuracy
@@ -117,5 +127,4 @@ postResample(test_data$label,multinomtest_result_stepwise)
 # 0.8033333 0.7048437 
 # -> error rate 19.67%
 
-#confusionMatrix(test_data$label,multinomtest_result)
 
